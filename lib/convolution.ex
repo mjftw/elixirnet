571726abd20afeb29b6195868fa04120114fa7df
Merlin Webster <mjftwebster@gmail.com>
2020-11-14 16:35:52 +0000

    Refactored to much faster convolution solution

--- lib/convolution.ex
+++ lib/convolution.ex
@@ -9,7 +9,7 @@ defmodule Convolution do
   Perform a sliding window style convoltion over an input matrix and a kernel matrix
   using "same"/"same" padding
   """
-  @spec convolve(Matrex.t(), Matrex.t()):: Matrex.t()
+  @spec convolve(Matrex.t(), Matrex.t()) :: Matrex.t()
   def convolve(input, kernel), do: convolve(input, kernel, :same)
 
   @doc """
@@ -20,19 +20,16 @@ defmodule Convolution do
   def convolve(input, kernel, :same) do
     input_padded = Padding.same(input, kernel)
 
+    row_offset = Padding.num_pad_rows(input, kernel, 1)
+    col_offset = Padding.num_pad_cols(input, kernel, 1)
+
     # Slide a kernel sized window over the input matrix and perform dot product
     # between the windowed matrix section and the convolution kernel at each location.
     # Each dot product forms the new value for the output matrix.
-    input
-    |> Matrex.Extra.coordinates()
-    |> Parallel.pmap(
-      fn {row, col} -> {
-          row + Padding.num_pad_rows(input, kernel, 1),
-          col + Padding.num_pad_cols(input, kernel, 1)}
-        |> (fn coord -> Matrex.Extra.submatrix_at(input_padded, coord, kernel[:size]) end).()
-        |> Matrex.multiply(kernel)
-        |> Matrex.sum()
-      end)
-    |> Matrex.reshape(input[:rows], input[:cols])
+    Matrex.apply(input, fn (_, row, col) ->
+      Matrex.Extra.submatrix_at(input_padded, {row + row_offset - 1, col + col_offset - 1}, kernel[:size])
+      |> Matrex.multiply(kernel)
+      |> Matrex.sum()
+    end)
   end
 end
