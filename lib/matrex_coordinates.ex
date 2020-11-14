dbc6378a5724ff35fe75184249b3c29e28efc28c
Merlin Webster <mjftwebster@gmail.com>
2020-11-14 00:12:21 +0000

    Added function to get a submatrix at a given coordinate and shape

--- lib/matrex_coordinates.ex
+++ lib/matrex_coordinates.ex
@@ -5,4 +5,23 @@ defmodule Matrex.Coordinates do
     |> Enum.with_index()
     |> Enum.map(fn {_, idx} -> {div(idx, matrix[:cols]), rem(idx, matrix[:cols])} end)
   end
+
+  @spec submatrix_at(
+          Matrex.t(),
+          {non_neg_integer, non_neg_integer},
+          {non_neg_integer, non_neg_integer}
+        ) :: Matrex.t()
+  def submatrix_at(_, _, {width, height}) when rem(width, 2) == 0 or rem(height, 2) == 0 do
+    raise "Submatrix width and height must be odd numbers"
+  end
+
+  def submatrix_at(matrix, {center_x, center_y}, {width, height}) do
+    x_sides = (width - 1) / 2
+    y_sides = (height - 1) / 2
+
+    matrix
+    |> Matrex.submatrix(
+    trunc(center_x + 1 - x_sides)..trunc(center_x + 1 + x_sides),
+    trunc(center_y + 1 - y_sides)..trunc(center_y + 1 + y_sides))
+  end
 end
--- test/matrex_coordinates_test.exs
+++ test/matrex_coordinates_test.exs
@@ -5,4 +5,43 @@ defmodule Matrex.CoordinatesTest do
   test "coordinates/1 returns correct values" do
     assert Matrex.zeros(2, 3) |> coordinates() == [{0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}, {1, 2}]
   end
+
+  test "submatrix_at/3 returns correct values" do
+    matrix = 1..8 * 8
+    |> Enum.to_list
+    |> Matrex.reshape(8, 8)
+
+    submatrix = matrix
+    |> submatrix_at({4, 5}, {3, 5})
+
+    expected = [
+      28.0, 29.0, 30.0, 31.0, 32.0,
+      36.0, 37.0, 38.0, 39.0, 40.0,
+      44.0, 45.0, 46.0, 47.0, 48.0
+    ]
+    |> Matrex.reshape(3, 5)
+
+    assert submatrix == expected
+  end
+
+  test "submatrix_at/3 raises error on out of bounds access" do
+    assert_raise RuntimeError, fn ->
+      Matrex.zeros(5)
+      |> submatrix_at({0, 0}, {3, 3})
+    end
+  end
+
+  test "submatrix_at/3 raises error on even submatrix rows" do
+    assert_raise RuntimeError, fn ->
+      Matrex.zeros(5)
+      |> submatrix_at({2, 2}, {2, 3})
+    end
+  end
+
+  test "submatrix_at/3 raises error on even submatrix cols" do
+    assert_raise RuntimeError, fn ->
+      Matrex.zeros(5)
+      |> submatrix_at({2, 2}, {3, 2})
+    end
+  end
 end
