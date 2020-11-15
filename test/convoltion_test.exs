d423671ac1ee696c88d94d627a78f2386cafc1c4
Merlin Webster <mjftwebster@gmail.com>
2020-11-15 08:46:00 +0000

    Added test for convolve/2

--- test/convoltion_test.exs
+++ test/convoltion_test.exs
@@ -15,7 +15,7 @@ defmodule ConvolutionTest do
       4.0, 6.0, 6.0, 6.0, 4.0
     ] |> Matrex.reshape(5, 5)
 
-    assert Convolution.convolve(input, kernel, :same) == expected
+    assert convolve(input, kernel, :same) == expected
   end
 
   test "convolve/3 returns correct output with :same padding, accending values" do
@@ -30,28 +30,35 @@ defmodule ConvolutionTest do
       320.0, 436.0, 457.0, 478.0, 280.0
     ] |> Matrex.reshape(5, 5)
 
-    assert Convolution.convolve(input, kernel, :same) == expected
+    assert convolve(input, kernel, :same) == expected
   end
 
   test "convolve/3 returns output with correct size on large matrices with :same padding" do
     input = Matrex.zeros(262, 191)
     kernel = Matrex.zeros(19, 27)
 
-    assert Convolution.convolve(input, kernel, :same)[:size] == input[:size]
+    assert convolve(input, kernel, :same)[:size] == input[:size]
+  end
+
+  test "convolve/2 uses :valid padding" do
+    input  = 1..5 * 5 |> Enum.to_list |> Matrex.reshape(5, 5)
+    kernel = 1..3 * 3 |> Enum.to_list |> Matrex.reshape(3, 3)
+
+    assert convolve(input, kernel) == convolve(input, kernel, :valid)
   end
 
   test "convolve/3 returns correct output with correct size with :valid padding, ones" do
     input  = Matrex.ones({5, 5})
     kernel = Matrex.ones({3, 3})
 
-    assert Convolution.convolve(input, kernel, :valid)[:size] == {3, 3}
+    assert convolve(input, kernel, :valid)[:size] == {3, 3}
   end
 
   test "convolve/3 returns correct output :valid padding, ones" do
     input  = Matrex.ones({5, 5})
     kernel = Matrex.ones({3, 3})
 
-    assert Convolution.convolve(input, kernel, :valid) == Matrex.fill(3, 9.0)
+    assert convolve(input, kernel, :valid) == Matrex.fill(3, 9.0)
   end
 
   test "convolve/3 returns correct output with :valid padding, accending values" do
@@ -64,7 +71,7 @@ defmodule ConvolutionTest do
       861.0, 906.0, 951.0
     ] |> Matrex.reshape(3, 3)
 
-    assert Convolution.convolve(input, kernel, :valid) == expected
+    assert convolve(input, kernel, :valid) == expected
   end
 
 end
